92594458a943d0c935f178bc4a03d5d5d82a20c2
koalaroo7 <39775320+koalaroo7@users.noreply.github.com>
2018-10-24 11:34:16 -0400

    Delete melGit.R

--- melGit.R
+++
@@ -1,7 +0,0 @@
-Melbourne_housing_FULL <- read.csv("~/Desktop/Melbourne data project/melbourne-housing-market/Melbourne_housing_FULL.csv")
-
-melHouse <- Melbourne_housing_FULL
-
-# check NA
-
-colSums(is.na(melHouse))
